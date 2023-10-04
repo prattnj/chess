be156cc693918b39aa33e2129ed87b72f8934cd1
Noah Pratt <prattnj@gmail.com>
2023-10-04 10:54:07 -0600

    Install new npm package

--- web/src/App.js
+++ web/src/App.js
@@ -7,20 +7,21 @@ function App() {
 }
 
 function LoginScreen() {
-  return (
-      <>
-        <div className={"login-all"}>
-          <span>Welcome!</span>
-          <input type={"text"}/>
-          <input type={"text"}/>
-          <div className={"login-buttons"}>
-            <div>Login</div>
-            <div>Register</div>
-          </div>
-        </div>
-      </>
 
-  )
+    return (
+        <>
+            <div className={"login-all"}>
+                <span>Welcome!</span>
+                <input className={"login-input"} type={"text"}/>
+                <input className={"login-input"} type={"text"}/>
+                <input className={"login-input"} type={"text"}/>
+                <div className={"login-buttons"}>
+                    <div className={"login-button"}>Login</div>
+                    <div className={"login-button"}>Register</div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
